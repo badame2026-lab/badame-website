8943a1fa470aabaf02a219485a8272a10458bd04
badame2026-lab <badame2026@gmail.com>
2026-01-19 00:11:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,10 @@
+function App() {
+  return (
+    <div style={{ background: "red", color: "white", padding: "40px" }}>
+      <h1>APP.JSX IS WORKING</h1>
+    </div>
+  );
+}
+
+export default App;
 
